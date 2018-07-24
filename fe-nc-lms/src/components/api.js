32a4e8b089ec664c491a15d53bb19c91c1d4d727
sco7 <sco77.townson@gmail.com>
2018-07-24 15:08:44 +0100

    Started to update url links to point to deployed backend

--- fe-nc-lms/src/components/api.js
+++ fe-nc-lms/src/components/api.js
@@ -1,5 +1,6 @@
 export const getLogin = (username, password) => {
-  const url = `http://localhost:3000/api/user/username/${username}`;
+  const url = `https://nc-be-lms.herokuapp.com/api/user/username/${username}`;
+  
   return fetch(url).then(res => {
     if (res.status === 404) throw new Error(res.statusText);
     return res.json();
@@ -84,4 +85,5 @@ export const getAllUsers = () => {
     return res.json();
   });
 };
+
 
